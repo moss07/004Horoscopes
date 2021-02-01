f7fed031c740752e67eba6a88ecbc56a57d18be1
ERKAN TUNCAY KARA <erkantuncaykara@gmail.com>
2021-02-01 10:04:16 +0300

    Add project files.

---
+++ 004Horoscopes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _004Horoscopes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Enter a value between 1-12 :  ");
+            int number = int.Parse(Console.ReadLine());
+
+            Horoscopes horoscope;
+            horoscope = (Horoscopes)number;
+
+            Console.WriteLine("Your horoscope is :  " + horoscope);
+        }
+
+        enum Horoscopes
+        {
+            Capricorn = 1,
+            Aquarius,
+            Pisces,
+            Aries,
+            Taurus,
+            Gemini,
+            Cancer,
+            Leo,
+            Virgo,
+            Libra,
+            Scorpio,
+            Sagittarius
+        }
+    }
+}
